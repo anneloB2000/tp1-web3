fb9c925821cc1b7dddd527e2262a6b320af2b950
Anne-Laurie Bélanger <anne.lo@hotmail.ca>
2026-09-16 16:50:32 -0400

    Ajout de la page Details et correction du code de Collection pour que le lien fonctionne

--- app.py
+++ app.py
@@ -22,12 +22,30 @@ def collection():
 
     with bd.creer_connexion() as conn:
         with conn.get_curseur() as curseur:
-            collection_carte = curseur.execute("select * from elements_collection")
+            curseur.execute("select * from elements_collection")
+            collection_carte = curseur.fetchall()
 
     return render_template('collection.jinja', collection_carte=collection_carte)
 
 
 
 
+@app.route('/details-carte')
+def details_carte():
+    """Affiche les détails d'une carte"""
+    identifiant = request.args.get('id', type=int)
+    carte = {}
+
+    with bd.creer_connexion() as conn:
+        with conn.get_curseur() as curseur:
+            curseur.execute(
+             'SELECT * FROM carte_pokemon JOIN elements_collection ON carte_pokemon.id = elements_collection.id_carte WHERE elements_collection.id = %(id)s',
+                {
+                    'id': identifiant
+                }
+            )
+            carte = curseur.fetchone()
+
+    return render_template('detail-carte.jinja', carte=carte)
 
 app.run(debug=True)
